89f693d86e5dcd7c387d86f0a0bf23feda5ce680
MiSerhii <miserhii@gmail.com>
2020-07-22 12:49:38 +0300

    Changes:
    - Connected FontAwesome
    - Divided index.scss/html
    - Created SearchForm

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,19 +1,24 @@
-jQuery(function($) {
-    "use strict"
+$(document).ready(function() {
+    var windowWidth = $(window).width();
+    var countSlides = $('.slide').length;
+    $('.slider_container').width((windowWidth*countSlides) + 'px');
+    $('.slide-dot').click(function() {
+        var slide = $(this).attr('data-slide');
+        $('.slider_container').css('transform', 'translate(-' + (slide*windowWidth) + 'px)');
+        $('.slide-dot').removeClass('active');    
+        $(this).addClass('active');
+    });
+    
+    $(window).resize(function() {
+        
+    });
 
-    $(document).ready(function() {
-        var windowWidth = $(window).width();
-        var countSlides = $('.slide').length;
-        $('.slider_container').width((windowWidth*countSlides) + 'px');
-        $('.slide-dot').click(function() {
-            var slide = $(this).attr('data-slide');
-            $('.slider_container').css('transform', 'translate(-' + (slide*windowWidth) + 'px)');
-            $('.slide-dot').removeClass('active');    
-            $(this).addClass('active');
-        });
-        $(window).resize(function() {
-            
-        });
+    var $searchIcon = $('.search_butt');
+    var $searchInput = $('.search_inpt');
+
+    $searchIcon.on('click', function() {
+        $searchInput.toggleClass('active');
     });
 });
+
 
