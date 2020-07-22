b66141333b4c5c2c901ec98c3fa23660f903624c
MiSerhii <miserhii@gmail.com>
2020-07-22 23:25:16 +0300

    Changes:
    - Connected slick-slider
    - +hover on search
    - Added some styles for seatch-input

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,16 +1,28 @@
 $(document).ready(function() {
-    var windowWidth = $(window).width();
-    var countSlides = $('.slide').length;
-    $('.slider_container').width((windowWidth*countSlides) + 'px');
-    $('.slide-dot').click(function() {
-        var slide = $(this).attr('data-slide');
-        $('.slider_container').css('transform', 'translate(-' + (slide*windowWidth) + 'px)');
-        $('.slide-dot').removeClass('active');    
-        $(this).addClass('active');
-    });
+    // var windowWidth = $(window).width();
+    // var countSlides = $('.slide').length;
+    // $('.slider_container').width((windowWidth*countSlides) + 'px');
+    // $('.slide-dot').click(function() {
+    //     var slide = $(this).attr('data-slide');
+    //     $('.slider_container').css('transform', 'translate(-' + (slide*windowWidth) + 'px)');
+    //     $('.slide-dot').removeClass('active');    
+    //     $(this).addClass('active');
+    // });
     
-    $(window).resize(function() {
+    // $(window).resize(function() {
         
+    // });
+    
+
+    $('.slider').slick({
+        arrows: false,
+        dots: true
+        // adaptiveHeight: true,
+        // autoplay: true,
+        // autoplaySpeed: 4000,
+        // pauseOnFocus: true,
+        // pauseOnHover: true,
+        // pauseOnDotHover: true
     });
 
     var $searchIcon = $('.search_butt');
@@ -19,6 +31,13 @@ $(document).ready(function() {
     $searchIcon.on('click', function() {
         $searchInput.toggleClass('active');
     });
+
+    // $('.search_inpt').on('keypress', function (event) {
+    //     if (event.keyCode == 13) {
+    //         event.preventDefault();
+    //         $('.search_inpt').append('default');
+    //     }
+    // });
 });
 
 
