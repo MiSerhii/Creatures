c074f421681d8f41c7029c4f615a64ce4cc9b9fe
MiSerhii <miserhii@gmail.com>
2020-07-23 00:26:01 +0300

    Changes:
    - Some new in search-form
    - Some changes in slider settings

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,28 +1,9 @@
 $(document).ready(function() {
-    // var windowWidth = $(window).width();
-    // var countSlides = $('.slide').length;
-    // $('.slider_container').width((windowWidth*countSlides) + 'px');
-    // $('.slide-dot').click(function() {
-    //     var slide = $(this).attr('data-slide');
-    //     $('.slider_container').css('transform', 'translate(-' + (slide*windowWidth) + 'px)');
-    //     $('.slide-dot').removeClass('active');    
-    //     $(this).addClass('active');
-    // });
     
-    // $(window).resize(function() {
-        
-    // });
-    
-
     $('.slider').slick({
         arrows: false,
-        dots: true
-        // adaptiveHeight: true,
-        // autoplay: true,
-        // autoplaySpeed: 4000,
-        // pauseOnFocus: true,
-        // pauseOnHover: true,
-        // pauseOnDotHover: true
+        dots: true,
+        slidesToShow: 1
     });
 
     var $searchIcon = $('.search_butt');
@@ -32,12 +13,22 @@ $(document).ready(function() {
         $searchInput.toggleClass('active');
     });
 
-    // $('.search_inpt').on('keypress', function (event) {
-    //     if (event.keyCode == 13) {
-    //         event.preventDefault();
-    //         $('.search_inpt').append('default');
-    //     }
-    // });
+    $searchInput.on('keypress', function (event) {
+        if (event.keyCode == 13) {
+            event.preventDefault();
+
+            $(this).val('');
+            $(this).blur();
+        }
+    });
+
+    $(document).on('click', function(event) { 
+        var $target = $(event.target);
+  
+        if (!$target.closest('.search').length) {
+          $searchInput.removeClass('active');
+        }
+      });
 });
 
 
